e9d604efdae547f86bf38cad6f1d72d359b4965c
Spirillen <pdns@protonmail.com>
2019-05-26 18:58:33 +0200

    Uploaded.. Sun 26 May 18:58:33 CEST 2019

--- tools/hosts_fetch.bash
+++ tools/hosts_fetch.bash
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash --verbose
 
-curltemp=/tmp/curltemp
+curltemp=$(mktemp /tmp/curl_temp.XXXXXXXXXX) || { echo "Failed to create temp file"; exit 1; }
+chmod +x {$curltemp,,}
+trap "rm -f $curltemp" 0 2 3 15
 
 curl -sSL  --compressed "https://github.com/FadeMind/hosts.extras/raw/master/hpHosts/hosts" | grep -v '^#' | grep -v '^-' | grep -v 'localhost' | cut -f2 >> {$curltemp,,}
 curl -sSL  --compressed "https://github.com/FadeMind/hosts.extras/raw/master/rlwpx.free.fr.hrsk/hosts" | grep -v '^#' | grep -v '^-' | grep -v 'localhost' | cut -d' ' -f2 >> {$curltemp,,}
@@ -8,14 +10,14 @@ curl -sSL  --compressed "https://gitlab.com/quidsup/notrack-blocklists/raw/maste
 curl -sSL  --compressed "https://hostsfile.mine.nu/hosts0.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d' |cut -f2 >> {$curltemp,,}
 curl -sSL  --compressed "https://hosts-file.net/ad_servers.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
 curl -sSL  --compressed "https://hosts-file.net/download/hosts.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/emd.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/exp.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/fsa.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/grm.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/hjk.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/mmt.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/psh.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://hosts-file.net/pup.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | cut -f2 >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/emd.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/exp.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/fsa.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/grm.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/hjk.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/mmt.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/psh.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://hosts-file.net/pup.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost$/d' | awk '{print $2}' >> {$curltemp,,}
 curl -sSL  --compressed "https://hostsfile.org/Downloads/hosts.txt" | sed -e 's/#.*$//g; s/^ *//; s/ *$//; /^$/d; /^\s*$/d; /localhost/d' | cut -f2 >> {$curltemp,,}
 curl -sSL  --compressed "https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt" | sed -e 's/#.*$//g; /^\s*$/d' >> {$curltemp,,}
 curl -sSL  --compressed "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0" >> {$curltemp,,}
@@ -25,28 +27,28 @@ curl -sSL  --compressed "https://ransomwaretracker.abuse.ch/downloads/LY_C2_DOMB
 curl -sSL  --compressed "https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt" | sed -e 's/#.*$//g; /^\s*$/d' >> {$curltemp,,}
 curl -sSL  --compressed "https://ransomwaretracker.abuse.ch/downloads/TC_C2_DOMBL.txt" | sed -e 's/#.*$//g; /^\s*$/d' >> {$curltemp,,}
 curl -sSL  --compressed "https://ransomwaretracker.abuse.ch/downloads/TL_C2_DOMBL.txt" | sed -e 's/#.*$//g; /^\s*$/d' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt" | sed -e 's/#.*$//g; /^\s*$/d' | cut -d' ' -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/CoinMiner.txt" | sed -e 's/#.*$//g; /^\s*$/d' | cut -d' ' -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt" | sed -e 's/#.*$//g; /^\s*$/d' | cut -d' ' -f2 >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt" | sed -e 's/#.*$//g; /^\s*$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/CoinMiner.txt" | sed -e 's/#.*$//g; /^\s*$/d' | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt" | sed -e 's/#.*$//g; /^\s*$/d' | awk '{print $2}' >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/anudeepND/youtubeadsblacklist/master/domainlist.txt" >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt" | sed -e 's/#.*$//g; /^\s*$/d' | cut -d' ' -f2 >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt" | sed -e 's/#.*$//g; /^\s*$/d' | awk '{print $2}' >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/Dawsey21/Lists/master/main-blacklist.txt" >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/HorusTeknoloji/TR-PhishingList/master/url-lists.txt" >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/matomo-org/referrer-spam-blacklist/master/spammers.txt" >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt" | sed -e 's/#.*$//g; /^\s*$/d' >> {$curltemp,,}
 curl -sSL  --compressed "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt" | sed -e 's/#.*$//g; /^\s*$/d; s/\/\///g' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.2o7Net/hosts" | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Risk/hosts" | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Spam/hosts" | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/KADhosts/hosts" | sed -e 's/#.*$//g; /^\s*$/d; s/\/\///g' | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/UncheckyAds/hosts" | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" | grep -v 'localhost' | grep -v 'ip6-' | grep -v local | grep -v broadcasthost| sed -e 's/#.*$//g; /^\s*$/d; s/\/\///g' | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://raw.githubusercontent.com/vokins/yhosts/master/hosts" | grep -v '#' | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://reddestdream.github.io/Projects/MinimalHosts/etc/MinimalHostsBlocker/minimalhosts" | grep -v '::' | grep -v '#' | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.2o7Net/hosts" | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Risk/hosts" | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Spam/hosts" | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/KADhosts/hosts" | sed -e 's/#.*$//g; /^\s*$/d; s/\/\///g' | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/UncheckyAds/hosts" | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" | grep -v 'localhost' | grep -v 'ip6-' | grep -v local | grep -v broadcasthost| sed -e 's/#.*$//g; /^\s*$/d; s/\/\///g' | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://raw.githubusercontent.com/vokins/yhosts/master/hosts" | grep -v '#' | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://reddestdream.github.io/Projects/MinimalHosts/etc/MinimalHostsBlocker/minimalhosts" | grep -v '::' | grep -v '#' | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt" >> {$curltemp,,}
 curl -sSL  --compressed "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt" | grep -v '#' | grep -v 'Malvertising list by Disconnect' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt" | grep -v '#' | grep -v 'Malvertising list by Disconnect' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "https://someonewhocares.org/hosts/zero/hosts" | grep -v '#' | grep -v '127.0.0.1' | grep -v '::' | grep -v 'broadcasthost'  | cut -d' ' -f2 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
+curl -sSL  --compressed "https://someonewhocares.org/hosts/zero/hosts" | grep -v '#' | grep -v '127.0.0.1' | grep -v '::' | grep -v 'broadcasthost'  | awk '{print $2}' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://v.firebog.net/hosts/AdAway.txt" | grep -v 'localhost' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://v.firebog.net/hosts/AdguardDNS.txt" | grep -v 'localhost' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://v.firebog.net/hosts/Airelle-trc.txt" | grep -v 'localhost' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
@@ -83,11 +85,11 @@ curl -sSL  --compressed "https://www.joewein.net/dl/bl/dom-bl-base.txt" | cut -d
 curl -sSL  --compressed "https://www.malwaredomainlist.com/hostslist/hosts.txt" | dos2unix | grep -v '#' | grep -iv 'localhost' | cut -d' ' -d' ' -f3 | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://www.squidblacklist.org/downloads/dg-ads.acl" | grep -v '#' >> {$curltemp,,}
 curl -sSL  --compressed "https://www.squidblacklist.org/downloads/dg-malicious.acl" | grep -v '#' >> {$curltemp,,}
-curl -sSL  --compressed "http://sysctl.org/cameleon/hosts" | grep -v '#' | grep -vi 'localhost' | cut -f2 | cut -d' ' -f2 >> {$curltemp,,}
-curl -sSL  --compressed "https://zerodot1.gitlab.io/CoinBlockerLists/hosts" | grep -v '#' | cut -d' ' -f2 >> {$curltemp,,}
+curl -sSL  --compressed "http://sysctl.org/cameleon/hosts" | grep -v '#' | grep -vi 'localhost' | cut -f2 | awk '{print $2}' >> {$curltemp,,}
+curl -sSL  --compressed "https://zerodot1.gitlab.io/CoinBlockerLists/hosts" | grep -v '#' | awk '{print $2}' >> {$curltemp,,}
 curl -sSL  --compressed "https://zeustracker.abuse.ch/blocklist.php?download=baddomains" | grep -v '#' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
 curl -sSL  --compressed "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist" | grep -v '#' | awk '/[a-z]$/{print $0}' >> {$curltemp,,}
-curl -sSL  --compressed "http://winhelp2002.mvps.org/hosts.txt" | dos2unix | grep -v '#' | grep -vi 'localhost' | awk '/[a-z]$/{print $0}' | cut -d' ' -f2 >> {$curltemp,,}
+curl -sSL  --compressed "http://winhelp2002.mvps.org/hosts.txt" | dos2unix | grep -v '#' | grep -vi 'localhost' | awk '/[a-z]$/{print $0}' | awk '{print $2}' >> {$curltemp,,}
 
 wget -qO- "https://github.com/FadeMind/hosts.extras/raw/master/add.Risk/hosts" | cut -d ' ' -f 2 | sort -u >> {$curltemp,,}
 wget -qO- "https://github.com/FadeMind/hosts.extras/raw/master/add.Spam/hosts" | cut -d ' ' -f 2 | sort -u >> {$curltemp,,}
@@ -101,6 +103,7 @@ wget -qO- "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts?inline=
 wget -qO- "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts" | cut -d ' ' -f 2 | sort -u >> {$curltemp,,}
 wget -qO- "https://raw.githubusercontent.com/jawz101/MobileAdTrackers/master/hosts" | cut -d ' ' -f 2 | sort -u >> {$curltemp,,}
 
-cat $curltemp | grep -i 'doubleclick.com' | sort -u > doubleclick.com.list
-
+cat $curltemp | sort -u > hosts
 rm -f $curltemp
+
+#cat hosts | grep -i 'igoogle.com' | sort -u > ../domains/igoogle.com.list
