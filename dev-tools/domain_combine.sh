f44942aed8d1d03ec79227da9e57d8dc75ab41e7
Spirillen <3728965-spirillen@users.noreply.gitlab.com>
2019-10-03 23:14:21 +0200

    Did we fix travis v.12

--- dev-tools/domain_combine.sh
+++ dev-tools/domain_combine.sh
@@ -32,6 +32,20 @@ rm $TLD
 #head domain.list
 head -n 5 ${TRAVIS_BUILD_DIR}/dev-tools/domain.test
 
+# *********************************************
+# Get Travis CI Prepared for Committing to Repo
+# *********************************************
+
+PrepareTravis () {
+    git remote rm origin
+    git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+    git config --global user.email "${GIT_EMAIL}"
+    git config --global user.name "${GIT_NAME}"
+    git config --global push.default simple
+    git checkout "${GIT_BRANCH}"
+    ulimit -u
+}
+PrepareTravis
 
 # ******************
 # Set our Input File
