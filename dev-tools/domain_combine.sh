613ef32d8627d6f67f10b390f8f4a95847a7eb05
Spirillen <3728965-spirillen@users.noreply.gitlab.com>
2019-10-03 14:52:25 +0200

    Pyf....

--- dev-tools/domain_combine.sh
+++ dev-tools/domain_combine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-yeartag=$(date +%Y)
-monthtag=$(date +%m)
+#yeartag=$(date +%Y)
+#monthtag=$(date +%m)
 
 #input=domain.list
 
@@ -33,10 +33,43 @@ head domain.list
 
 cd dev-tools/
 
-PyFunceble --travis -db -ex --autosave-minutes 20 --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Google temp test]" \
+# ******************
+# Set our Input File
+# ******************
+
+pyfuncebleConfigurationFileLocation=${CI_PROJECT_DIR}/dev-tools/.PyFunceble.yaml
+pyfuncebleProductionConfigurationFileLocation=${CI_PROJECT_DIR}/dev-tools/.PyFunceble_production.yaml
+
+# Run PyFunceble Testing
+RunFunceble () {
+
+    yeartag=$(date +%Y)
+    monthtag=$(date +%m)
+    ulimit -u
+    cd ${TRAVIS_BUILD_DIR}/dev-tools
+
+    hash PyFunceble
+
+    if [[ -f "${pyfuncebleConfigurationFileLocation}" ]]
+    then
+        rm "${pyfuncebleConfigurationFileLocation}"
+        rm "${pyfuncebleProductionConfigurationFileLocation}"
+    fi
+
+#    PyFunceble --travis -db -ex --dns 95.216.209.53 116.203.32.67 \
+#        --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
+#        --plain --autosave-minutes 20 \
+#        --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Auto Saved]" \
+#        --commit-results-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}" -f ${input}
+
+
+PyFunceble --travis -db -ex -dbr 30 --autosave-minutes 20 --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Google temp test]" \
 	--commit-results-message "Live google domains tested ${yeartag}.${monthtag}.`date +%s`" \
 	--cmd-before-end "bash FinalCommit.sh" -f ../domain.list
 
 #cat output/domains/ACTIVE/list | awk '/^#/{ next }; { printf("%s\n",tolower($1)) }' >> ?
+}
+
+RunFunceble
 
 exit ${?}
