60b6dfd0e970bef740bb9199d0d831fdbea1b476
Spirillen <3728965-spirillen@users.noreply.gitlab.com>
2019-10-03 04:03:41 +0200

    Getting closer

--- dev-tools/domain_combine.sh
+++ dev-tools/domain_combine.sh
@@ -5,12 +5,12 @@ monthtag=$(date +%m)
 
 input=${CI_BUILDS_DIR}/domain.list
 
-rm ${input}
+#rm ${input}
 
 TLD=`(mktemp)`
 wget -qO- 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt' | awk '/^#/{ next }; { printf("%s\n",tolower($1))}' > ${TLD}
 
-mapfile -t a < domain.list
+mapfile -t a < ${CI_BUILDS_DIR}/google.list
 mapfile -t b < $TLD
 
 for((i=0;i<${#a[@]};i++));
@@ -29,8 +29,8 @@ done
 rm $TLD
 
 #PyFunceble --travis -db -ex --autosave-minutes 20 --commit-autosave-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [Google temp test]" \
-	--commit-results-message "Live google domains tested ${yeartag}.${monthtag}.`date +%s`" \
-	--cmd-before-end "bash ${CI_BUILDS_DIR}/dev-tools/FinalCommit.sh" -f ${input}
+#	--commit-results-message "Live google domains tested ${yeartag}.${monthtag}.`date +%s`" \
+#	--cmd-before-end "bash ${CI_BUILDS_DIR}/dev-tools/FinalCommit.sh" -f ${input}
 
 #cat output/domains/ACTIVE/list | awk '/^#/{ next }; { printf("%s\n",tolower($1)) }' >> ?
 
