1974aeb4621732ff3ef1f9afd7f1637609d3900d
spirillen <44526987+spirillen@users.noreply.github.com>
2019-10-05 12:16:12 +0200

    Update FinalCommit.sh

--- dev-tools/FinalCommit.sh
+++ dev-tools/FinalCommit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Created by: Spirillen
 # Copyright: Spirillen - https://gitlab.com/spirillen
 # Repo Url: https://gitlab.com/rpz-zones/google.tld
@@ -6,7 +6,7 @@
 
 TEMP=`(mktemp)`
 RPZ=`(mktemp)`
-ACTIVE=`(${TRAVIS_BUILD_DIR}/dev-tools/output/domains/ACTIVE/list)`
+ACTIVE=${TRAVIS_BUILD_DIR}/dev-tools/output/domains/ACTIVE/list
 
 cat ${ACTIVE} | awk '/^#/{ next }; { printf("%s\n",tolower($1)) }' >> ${TEMP}
 mv ${TEMP} ${TRAVIS_BUILD_DIR}/domains.list
@@ -14,5 +14,7 @@ mv ${TEMP} ${TRAVIS_BUILD_DIR}/domains.list
 printf "google-rpz.mypdns.cloud.\t3600\tIN\tSOA\tneed.to.know.only. hostmaster.mypdns.org. `date +%s` 3600 60 604800 60;\ngoogle-rpz.mypdns.cloud.\t3600\tIN\tNS\tlocalhost\n" > ${RPZ}
 cat ${ACTIVE} | awk '/^#/{ next }; {  printf("%s\tCNAME\t.\n*.%s\tCNAME\t.\n",tolower($1),tolower($1)) }' >> ${RPZ}
 mv ${RPZ} ${TRAVIS_BUILD_DIR}/google-rpz.mypdns.cloud.rpz
+
+git status
 
 exit ${?}
