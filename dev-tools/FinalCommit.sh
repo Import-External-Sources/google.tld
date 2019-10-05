a7b95b47ec33c907954571a79027a3f6720bc6d0
spirillen <44526987+spirillen@users.noreply.github.com>
2019-10-05 20:51:16 +0200

    Update FinalCommit.sh

--- dev-tools/FinalCommit.sh
+++ dev-tools/FinalCommit.sh
@@ -9,11 +9,15 @@ RPZ=`(mktemp)`
 ACTIVE=${TRAVIS_BUILD_DIR}/dev-tools/output/domains/ACTIVE/list
 
 cat ${ACTIVE} | awk '/^#/{ next }; { printf("%s\n",tolower($1)) }' >> ${TEMP}
+  head ${TEMP}
 mv ${TEMP} ${TRAVIS_BUILD_DIR}/domains.list
 
 printf "google-rpz.mypdns.cloud.\t3600\tIN\tSOA\tneed.to.know.only. hostmaster.mypdns.org. `date +%s` 3600 60 604800 60;\ngoogle-rpz.mypdns.cloud.\t3600\tIN\tNS\tlocalhost\n" > ${RPZ}
 cat ${ACTIVE} | awk '/^#/{ next }; {  printf("%s\tCNAME\t.\n*.%s\tCNAME\t.\n",tolower($1),tolower($1)) }' >> ${RPZ}
+  head ${RPZ}
 mv ${RPZ} ${TRAVIS_BUILD_DIR}/google-rpz.mypdns.cloud.rpz
+
+head ${TRAVIS_BUILD_DIR}/google-rpz.mypdns.cloud.rpz ${TRAVIS_BUILD_DIR}/domains.list
 
 git status
 
