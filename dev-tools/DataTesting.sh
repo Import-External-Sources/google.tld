6170ee0033160218e2cd420dc459d831421b27f7
spirillen <44526987+spirillen@users.noreply.github.com>
2020-02-04 21:31:08 +0100

    Update DataTesting.sh

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -3,18 +3,18 @@
 # ******************
 # Set our Input File
 # ******************
-input=${TRAVIS_BUILD_DIR}/dev-tools/domain.test
-pyfuncebleConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml
-pyfuncebleProductionConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble_production.yaml
+input="${TRAVIS_BUILD_DIR}/dev-tools/domain.test"
+pyfuncebleConfigurationFileLocation="${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml"
+pyfuncebleProductionConfigurationFileLocation="${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble_production.yaml"
 
 # Run PyFunceble Testing
 RunFunceble () {
 
     yeartag=$(date +%Y)
     monthtag=$(date +%m)
-    dateTime=$(date '+%F %X %:z (%Z)')
-    ulimit -u
-    cd ${TRAVIS_BUILD_DIR}/dev-tools
+    dateTime=$(date '+%F %T %:z (%Z)')
+    
+    cd "${TRAVIS_BUILD_DIR}/dev-tools"
 
     hash PyFunceble
 
@@ -24,11 +24,14 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-   PyFunceble --ci -ex -dbr 30 --dns 127.0.0.1 --http --plain \
+   PyFunceble --ci -ex -dbr 30 --dns 127.0.0.1 --http -h --plain \
+        --autosave-minutes 20 \
+        --ci-branch  master \
+        --ci-distribution-branch master \
+        --commit-autosave-message "${dateTime} ${TRAVIS_BUILD_NUMBER} [Stop Google]" \
+        --commit-results-message "Live google domains tested ${dateTime} ${TRAVIS_BUILD_NUMBER}" \
         --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
-        --autosave-minutes 20 --commit-autosave-message "${dateTime} v.${TRAVIS_BUILD_NUMBER} [Stop Google]" \
-        --commit-results-message "Live google domains tested ${dateTime} v.${TRAVIS_BUILD_NUMBER} [skip ci]" \
-        --ci-branch  master --ci-distribution-branch master -f ${input}
+        -f ${input}
 }
 
 RunFunceble
